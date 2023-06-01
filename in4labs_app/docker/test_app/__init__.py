90b5227fd616c7d607f397f9b35084dbce127e19
cRejon <carlosrejongomez@gmail.com>
2023-06-01 15:42:04 +0200

    Update Docker test app

---
+++ in4labs_app/docker/test_app/__init__.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from datetime import datetime, timedelta
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+@app.route('/')
+def index():
+    
+    tpl_kwargs = {
+        'user_email': os.environ['USER_EMAIL'],
+        'user_id': os.environ['USER_ID'],
+        'end_time': os.environ['END_TIME'],
+    }
+    return render_template('index.html', **tpl_kwargs)
+
+
