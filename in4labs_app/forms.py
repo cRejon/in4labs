3e9f73f143e5290ebbde8db4c2c8410aeed35e80
cRejon <carlosrejongomez@gmail.com>
2023-06-01 15:29:45 +0200

    Update forms

--- in4labs_app/forms.py
+++ in4labs_app/forms.py
@@ -1,26 +1,33 @@
-from flask_wtf import FlaskForm
-from wtforms import TimeField, SubmitField, DateField
-from wtforms.validators import ValidationError, DataRequired
-from in4labs_app.models import Reservation
 from datetime import datetime, time
 
+from flask_wtf import FlaskForm
+from wtforms import TimeField, SubmitField, DateField
+from wtforms.validators import DataRequired
 
-class ReservationForm(FlaskForm):
+
+class BookingForm(FlaskForm):
+
+    def __init__(self, lab_duration, *args, **kwargs):
+        super(BookingForm, self).__init__(*args, **kwargs)
+        self.lab_duration = lab_duration
+
     date = DateField('Date', 
                     format='%Y-%m-%d', 
                     default=datetime.now(),
                     validators=[DataRequired()])
     hour = TimeField('Hour', 
                     format='%H:%M', 
-                    default= time(hour=(datetime.now().hour), minute=0),
+                    default= time(hour=datetime.now().hour, 
+                                  minute=datetime.now().minute),
                     validators=[DataRequired()])
-    submit = SubmitField('Check')
+    submit = SubmitField('Check out')
 
-    def validate_time_slot(self, hour):
-        if hour.data.minute != 0:
-            raise ValidationError('Select an o´clock hour (eg. 14:00).')
+    def validate_hour(self, hour):
+        round_minute = hour.data.minute - (hour.data.minute % self.lab_duration)
+        self.hour.data = self.hour.data.replace(minute=round_minute)
         
-        date_time = datetime.combine(self.date.data, hour.data)
-        reservation = Reservation.query.filter_by(date_time=date_time).first()
-        if reservation is not None: # time slot is already reserved
-            raise ValidationError('This time slot is already reserved, please select a different one.')
+        
+
+
+
+    
