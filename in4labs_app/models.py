d9cfb06e1a27a9bb1eb5be893a0d06bdd02e05fc
cRejon <carlosrejongomez@gmail.com>
2024-03-30 00:32:33 +0100

    Change booking model

--- in4labs_app/models.py
+++ in4labs_app/models.py
@@ -17,6 +17,7 @@ def load_user(id):
 
 
 class Booking(db.Model):
-    id=db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
-    user_id=db.Column(db.Integer, nullable=False)
-    date_time=db.Column(db.DateTime, nullable=False, unique=True)
+    id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    lab_name = db.Column(db.String(64), nullable=False)
+    date_time = db.Column(db.DateTime, nullable=False, unique=True)
